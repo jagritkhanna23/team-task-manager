6aa5fc4736d44f766108191a743fd80e19eec8f0
jagritkhanna23 <jagritkhanna23@gmail.com>
2026-04-30 23:50:58 +0530

    + (Stage All Changes)

--- db.js
+++ db.js
@@ -1,15 +1,15 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE
 });
 
 db.connect(err => {
-  if (err) console.log(err);
-  else console.log("MySQL Connected");
+    if (err) console.log(err);
+    else console.log("MySQL Connected");
 });
 
 module.exports = db;
