981f14d5f77c2cedaece3c7080e5a9146f4b7c8e
jagritkhanna23 <jagritkhanna23@gmail.com>
2026-05-01 01:41:20 +0530

    + (Stage All Changes)

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const cors = require("cors");
 const express = require("express");
 const app = express();
 app.use(cors());
+ origin: "http://localhost:3000"
 const db = require("./db");
 
 app.use(express.json());
